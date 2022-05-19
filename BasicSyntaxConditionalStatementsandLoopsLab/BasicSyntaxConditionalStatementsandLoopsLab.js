8993cc012e49f3a404f018764bad91add15dbf50
VasilGVasilev <vaskomeil@gmail.com>
2022-05-19 21:27:53 +0300

    first lab

---
+++ BasicSyntaxConditionalStatementsandLoopsLab/BasicSyntaxConditionalStatementsandLoopsLab.js
@@ -0,0 +1,148 @@
+// muliply number by 2
+// function multiplyByTwo(num){
+//     console.log(num * 2)
+// }
+
+
+// student information
+// function studentInfo(name, age, grade) {
+//     console.log(`Name: ${name}, Age: ${age}, Grade: ${grade.toFixed(2)}`)
+// }
+
+// function grade(grade){
+//     if (grade >= 5.50) {
+//         console.log(`Excellent`)
+//     } else {
+//         console.log(`Not excellent`)
+//     }
+// }
+
+// foreign languages
+// function foreignLan(country) {
+//     if (country === `England` || country === `USA`) {
+//         console.log(`English`)
+//     } else if (country === `Spain` || country === `Argentina` || country === `Mexico`) {
+//         console.log(`Spanish`)
+//     } else {
+//         console.log(`unknown`)
+//     }
+// }
+
+// month printer
+// function printMonth(month) {
+//     if (month < 1) {
+//         console.log(`Error!`)
+//     } else if(month == 1) {
+//         console.log(`January`)
+//     } else if(month == 2) {
+//         console.log(`February`)
+//     } else if(month == 3) {
+//         console.log(`March`)
+//     } else if(month == 4) {
+//         console.log(`April`)
+//     } else if(month == 5) {
+//         console.log(`May`)
+//     } else if(month == 6) {
+//         console.log(`June`)
+//     } else if(month == 7) {
+//         console.log(`July`)
+//     } else if(month == 8) {
+//         console.log(`August`)
+//     } else if(month == 9) {
+//         console.log(`September`)
+//     } else if(month == 10) {
+//         console.log(`October`)
+//     } else if(month == 11) {
+//         console.log(`November`)
+//     } else if(month == 12) {
+//         console.log(`December`)
+//     } else if (month > 12) {
+//         console.log(`Error!`)
+//     }
+// }
+
+
+// theatre promo
+// function theatrePromo(day, age) {
+//     if (day == `Weekday`) {
+//         if (age < 0){
+//             console.log(`Error!`)
+//         } else if(age <= 18) {
+//             console.log(`12$`)
+//         } else if (age <= 64) {
+//             console.log(`18$`)
+//         } else if (age <= 122) {
+//             console.log(`12$`)
+//         } else if (age > 122){
+//             console.log(`Error!`)
+//         }
+//     } else if (day === `Weekend`) {
+//         if (age < 0){
+//             console.log(`Error!`)
+//         } else if(age <= 18){
+//             console.log(`15$`)
+//         } else if (age <= 64) {
+//             console.log(`20$`)
+//         } else if (age <= 122) {
+//             console.log(`15$`)
+//         } else if (age > 122){
+//             console.log(`Error!`)
+//         }
+//     } else if (day === `Holiday`) {
+//         if (age < 0){
+//             console.log(`Error!`)
+//         } else if(age <= 18){
+//             console.log(`5$`)
+//         } else if (age <= 64) {
+//             console.log(`12$`)
+//         } else if (age <= 122) {
+//             console.log(`10$`)
+//         } else if (age > 122){
+//             console.log(`Error!`)
+//         }
+//     }
+// }
+
+// numbers1to5
+// function numbers1to5() {
+//     for (let i = 1; i <= 5; i++){
+//         console.log(i)
+//     } 
+// }
+// numbers1to5()
+
+// divideby3
+// function divideby3() {
+//     for (let i = 1; i <= 100; i++){
+//         if (i % 3 == 0){
+//             console.log(i)
+//         }
+//     } 
+// }
+
+// from N to 1
+// function Nto1(n) {
+//     for (let i = n; i > 0; i--){
+//         console.log(i)
+//     } 
+// }
+
+// from M to N
+// function MtoN(m, n) {
+//     while (m>=n) {
+//         console.log(m)
+//         m--
+//     }
+// }
+
+// Sum of odds
+// function sumOfOdds(n){
+//     let sum = 0;
+//     let l = 1;
+//     for (let i = 1; i <= n; i++) {
+//         console.log(l);
+//         sum = sum + l;
+//         l = l + 2
+//     }
+//     console.log(`Sum: ${sum}`);
+// }
