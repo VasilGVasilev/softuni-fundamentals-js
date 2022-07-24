3138d4f521fab94cfe79aa47d076721131a6f5d9
Vasil Vasilev <vaskomeil@gmail.com>
2022-07-24 16:32:17 +0300

    7/10

--- AssociativeArrays/associativeArraysExe.js
+++ AssociativeArrays/associativeArraysExe.js
@@ -138,77 +138,240 @@
 
 
 
-function partyTime(input){
-    let guestList = [];
-    for (let el of input){
-        if (el === 'PARTY'){
-            break;
-        } else {
-            guestList.push(el);
-            input.shift();
-        }
-    }
-    input.shift();
+// function partyTime(input){
+//     let vip = [];
+//     let normal = [];
 
-    let partyGoers = input;
-    let guestListMap = new Map();
-    for (let guest of guestList){
-        guestListMap.set(guest, 1);
-    }
+//     let isParty = false;
 
+//     for (let el of input){
+//         if (el === 'PARTY'){
+//             isParty = true;
+//             continue;
+//         }
+//         if(!isParty){
+//             let firstCh = Number(el[0]);
+//             if(isNaN(firstCh)){
+//                 normal.push(el);
+//             } else{
+//                 vip.push(el);
+//             }
+//         } else{
+//             let firstCh = Number(el[0]);
+//             if (isNaN(firstCh)){
+//                 let index = normal.indexOf(el);
+//                 normal.splice(index, 1);
+//             } else {
+//                 let index = vip.indexOf(el);
+//                 vip.splice(index, 1)
+//             }
+//         }
+//     }
+//     let size = vip.length + normal.length;
+//     console.log(size);
+//     vip.forEach((el) => console.log(el));
+//     normal.forEach((el) => console.log(el));
+// }
 
+// partyTime(['m8rfQBvl',
+// 'fc1oZCE0',
+// 'UgffRkOn',
+// '7ugX7bm0',
+// '9CQBGUeJ',
+// '2FQZT3uC',
+// 'dziNz78I',
+// 'mdSGyQCJ',
+// 'LjcVpmDL',
+// 'fPXNHpm1',
+// 'HTTbwRmM',
+// 'B5yTkMQi',
+// '8N0FThqG',
+// 'xys2FYzn',
+// 'MDzcM9ZK',
+// 'PARTY',
+// '2FQZT3uC',
+// 'dziNz78I',
+// 'mdSGyQCJ',
+// 'LjcVpmDL',
+// 'fPXNHpm1',
+// 'HTTbwRmM',
+// 'B5yTkMQi',
+// '8N0FThqG',
+// 'm8rfQBvl',
+// 'fc1oZCE0',
+// 'UgffRkOn',
+// '7ugX7bm0',
+// '9CQBGUeJ'
+// ])
 
-    for (let guest of partyGoers){
-        if(guestListMap.has(guest)){
-            guestListMap.delete(guest);
-        }
-    }
+// Difference between Set(), Map(), Object() -> https://stackoverflow.com/questions/24085708/what-is-difference-between-map-and-set
+
+// runtime error 20/100 due to using split and making an array of string which should be made into a string only -> cards.toString();
+// function cardGame(input){
+//     let power = {
+//         '2': 2,
+//         '3': 3,
+//         '4': 4,
+//         '5': 5,
+//         '6': 6,
+//         '7': 7,
+//         '8': 8,
+//         '9': 9,
+//         '10': 10,
+//         'J': 11,
+//         'Q': 12,
+//         'K': 13,
+//         'A': 14
+//     };
+//     let type = {
+//         'S': 4,
+//         'H': 3,
+//         'D': 2,
+//         'C': 1
+//     };
+//     let people = {};
+//     let finalPeople = {};
+//     for (let el of input){
+
+//         let [name, gcards] = el.split(': ');
+
+//         cards = gcards.split(', ');
+        
+
+//         // populate object with all cards each player has
+//         if(!people.hasOwnProperty(name)){
+//             people[name] = cards.toString();
+//         } else {
+//             let currentCardsBatch = cards + ',' + people[name];
+//             people[name] = currentCardsBatch;
+//         }
+//     }
+
     
-    let arrOfGuests = Array.from(guestListMap.keys());
-    let vipGuests = [];
-    let plebs = [];
-    arrOfGuests.forEach((guest)=>{
-        let tempTest = Number(guest[0]);
-        if (isNaN(tempTest)){
-            plebs.push(guest);
-        } else {
-            vipGuests.push(guest);
-        }
-    })
-    console.log(arrOfGuests.length);
-    vipGuests.forEach(e=>console.log(e))
-    plebs.forEach(e=>console.log(e))
-}
+//     for (let card in people){
+//         let name = card;
+//         let arrOfCards = people[card].split(',');
+        
+//         let setOfPeople = new Set(arrOfCards);
+//         let arrOfSetOfPeople = Array.from(setOfPeople);
 
-partyTime(['m8rfQBvl',
-'fc1oZCE0',
-'UgffRkOn',
-'7ugX7bm0',
-'9CQBGUeJ',
-'2FQZT3uC',
-'dziNz78I',
-'mdSGyQCJ',
-'LjcVpmDL',
-'fPXNHpm1',
-'HTTbwRmM',
-'B5yTkMQi',
-'8N0FThqG',
-'xys2FYzn',
-'MDzcM9ZK',
-'PARTY',
-'2FQZT3uC',
-'dziNz78I',
-'mdSGyQCJ',
-'LjcVpmDL',
-'fPXNHpm1',
-'HTTbwRmM',
-'B5yTkMQi',
-'8N0FThqG',
-'m8rfQBvl',
-'fc1oZCE0',
-'UgffRkOn',
-'7ugX7bm0',
-'9CQBGUeJ'
-]
+//         let sumOfCards = 0;
+        
+//         arrOfSetOfPeople.forEach(specificCard => {
+//             // specificCard can be KD but also 10D
+            
+//             if (specificCard.length == 2){
+//                 let [p,t] = specificCard.split('');
+//                 sumOfCards+=(Number(power[p])*Number(type[t]));
+//             } else {
+//                 let [p,o,t] = specificCard.split('');
+//                 p+=o;
+//                 sumOfCards+=(Number(power[p])*Number(type[t]));
+//             }
+ 
+//         });
+        
+//         // update Object directly with sum as value
+//         if (!finalPeople.hasOwnProperty(name)) {
+//             finalPeople[name] = sumOfCards;
+//         } else {
+//             let currentSum = final[name] + sumOfCards;
+//             finalPeople[name] = currentSum;
+//         }
+//     }
+    
+//     for (let key in finalPeople) {
+//         console.log(`${key}: ${finalPeople[key]}`);
+//       }
+    
+    
+// }
 
-)
+
+// cardGame([
+//     'Peter: 2C, 4H, 9H, AS, QS',
+//     'Tomas: 3H, 10S, JC, KD, 5S, 10S',
+//     'Andrea: QH, QC, QS, QD',
+//     'Tomas: 6H, 7S, KC, KD, 5S, 10C',
+//     'Andrea: QH, QC, JS, JD, JC',
+//     'Peter: JD, JD, JD, JD, JD, JD'
+//     ]
+//     )
+
+
+
+
+// function companyUsers(input){
+//     let register = {};
+//     for (let el of input){
+//         let [name, id] = el.split(' -> ');
+//         let setForIds = new Set();
+
+//             if(!register.hasOwnProperty(name)){
+//                 register[name] = setForIds;
+//                 setForIds.add(id);
+//             } else {
+//                 register[name].add(id);
+//             }
+//     }
+//     let sortedArr = Object.keys(register).sort((a,b) => {
+//         return a.localeCompare(b)});
+//     for (let key of sortedArr){
+//         console.log(key);
+//         register[key].forEach(element => {
+//             console.log(`-- ${element}`);
+//         });
+//     }
+   
+// }
+
+// companyUsers([
+//     'SoftUni -> AA12345',
+//     'SoftUni -> CC12344',
+//     'Lenovo -> XX23456',
+//     'SoftUni -> AA12345',
+//     'Movement -> DD11111'
+//     ]
+    
+//     )
+
+
+
+// function minerTask(input){
+//     counter = 1;
+//     let inventory = {};
+//     let item = [];
+//     let quant = [];
+//     for (let el of input){
+//         if(counter % 2 !== 0){
+//             item.push(el);
+//         } else {
+//             quant.push(el);
+//         }
+//         counter++;
+//     }
+//     for (let i = 0; i < item.length; i++){
+//         if(!inventory.hasOwnProperty(item[i])){
+//             inventory[item[i]] = Number(quant[i]);
+//         } else {
+//             let currentVal = inventory[item[i]];
+//             currentVal+=Number(quant[i]);
+//             inventory[item[i]] = currentVal;
+//         }
+//     }
+//     for (let key in inventory){
+//         console.log(`${key} -> ${inventory[key]}`);
+//     }
+// }
+
+// minerTask([
+// 'gold',
+// '155',
+// 'silver',
+// '10',
+// 'copper',
+// '17',
+// 'gold',
+// '15'
+// ]
+//     )
