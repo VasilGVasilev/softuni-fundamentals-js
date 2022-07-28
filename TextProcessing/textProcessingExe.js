eaf40f8ef098c0f6f128f341d9a901dcaf0a00a2
Vasil Vasilev <vaskomeil@gmail.com>
2022-07-28 14:21:41 +0300

    finished

--- TextProcessing/textProcessingExe.js
+++ TextProcessing/textProcessingExe.js
@@ -123,3 +123,109 @@
 
 
 
+// function hardWord(input){
+//     let str = input.shift();
+//     let arr = input.shift();
+//     let arrOfStr = str.split(' ');
+//     let underscoreArr = [];
+    
+//     arr.forEach(word => {
+//         let searchedWord = '';
+//         for (let i = 0; i < word.length; i++){
+//             searchedWord+='_';
+//         }
+//         underscoreArr.push(searchedWord)
+        
+//     });
+
+//     // without sorting in decending order
+//     // there will be gaps for example
+//     // ______ could be filled with pie
+//     // because replace searches for ___ for pie, but _____ fits too => pie__;
+//     underscoreArr.sort((a,b) => b.length - a.length)
+//     arr.sort((a,b) => b.length - a.length)
+    
+//     for (let i = 0; i < arr.length; i++){
+//         str = str.replace(underscoreArr[i], arr[i])
+//     }
+//     console.log(str);
+    
+    
+// }
+
+// hardWord(['Hi, grandma! I\'m so ____ to write to you. ______ the winter vacation, so _______ things happened. My dad bought me a sled. Mom started a new job as a __________. My brother\'s ankle is ________, and now it bothers me even more. Every night Mom cooks ___ on your recipe because it is the most delicious. I hope this year Santa will _____ me a robot.',
+// ['pie', 'bring', 'glad', 'During', 'amazing', 'pharmacist', 'sprained']]);
+
+
+
+
+
+
+
+
+// function passGen(input){
+//     let [firstStr, secondStr, word] = input;
+//     let concatinated = firstStr.concat(secondStr);
+//     word = word.toUpperCase();
+  
+//     let counter = 0;
+//     for (let i = 0; i < concatinated.length; i++){
+//         if(concatinated[i] === 'a' || concatinated[i] === 'e' || concatinated[i] === 'i' || concatinated[i] === 'o' || concatinated[i] === 'u'){
+//             concatinated = concatinated.replace(concatinated[i], word[counter]);
+//             counter++;
+//         }
+//         if (counter === word.length){
+//             counter = 0;
+//         }
+//     }
+//     // let reversed = concatinated.split('').reverse().join('');
+   
+//     console.log(`Your generated passowrd is ${concatinated.split('').reverse().join('')}`);
+// }
+
+// passGen([
+//     'ilovepizza', 'ihatevegetables',
+//     'orange'
+//     ]
+//     );
+
+
+
+// function LettersChangeNums(input){
+//     let arr = input.split(' ');
+//     arr = arr.filter((e)=>e.length > 0);
+//     let arrOfNums = [];
+
+//     arr.forEach(element => {
+//         let rawWord = element.split('');
+//         let letterBefore = rawWord.shift();
+//         let letterAfter = rawWord.pop();
+//         let filteredNum = Number(rawWord.join(''));
+        
+//         if (letterBefore.charCodeAt(0) >= 65 && letterBefore.charCodeAt(0) <= 90){
+//             filteredNum /= (letterBefore.charCodeAt(0) - 64);
+//         }
+        
+//         if (letterBefore.charCodeAt(0) >= 97 && letterBefore.charCodeAt(0) <= 122){
+//             filteredNum *= (letterBefore.charCodeAt(0) - 96);
+//         }
+        
+//         if (letterAfter.charCodeAt(0) >= 65 && letterAfter.charCodeAt(0) <= 90){
+//             filteredNum -= (letterAfter.charCodeAt(0) - 64);
+//         }
+        
+//         if (letterAfter.charCodeAt(0) >= 97 && letterAfter.charCodeAt(0) <= 122){
+//             filteredNum += (letterAfter.charCodeAt(0) - 96);
+//         }
+        
+//         arrOfNums.push(filteredNum);
+        
+//     });
+    
+//     let sum = 0;
+//     arrOfNums.forEach(element => {
+//         sum+=element;
+//     });
+//     console.log(sum.toFixed(2));
+// }
+// LettersChangeNums('P34562Z q2576f   H456z');
